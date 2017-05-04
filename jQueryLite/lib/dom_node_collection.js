daba31003e2f293f764171e2da282a33b218cd92
Tu Ngo <tungo8810@gmail.com>
2017-05-04 14:33:06 -0700

    Implement attr, addClass, and append

--- jQueryLite/lib/dom_node_collection.js
+++ jQueryLite/lib/dom_node_collection.js
@@ -3,18 +3,59 @@ class DOMNodeCollection {
     this.elements = elements;
   }
 
+  attr(attribute, value) {
+    if (value === undefined) {
+      return this.elements[0].getAttribute(attribute);
+    } else {
+      this.elements.forEach((el) => {
+        el.setAttribute(attribute, value);
+      });
+    }
+  }
+
+  addClass(className) {
+    this.elements.forEach((el) => {
+      if (!el.className.includes(className)) {
+        el.className += ` ${className}`;
+      }
+    });
+  }
+
   html(innerHTML) {
     if (innerHTML === undefined) {
       return this.elements[0].innerHTML;
     } else {
-      this.elements.forEach( el => {
-        el.innerHTML = innerHTML;
-      });
+      this.appendInnerHTML(innerHTML, false);
     }
   }
 
   empty() {
     this.html('');
+  }
+
+  appendInnerHTML(content, isAppend = true) {
+    this.elements.forEach( el => {
+      if (isAppend) {
+        el.innerHTML += content;
+      } else {
+        el.innerHTML = content;
+      }
+    });
+  }
+
+  append(content) {
+    if (content instanceof DOMNodeCollection) {
+      const outerHTML = "";
+      content.elements.forEach((el) => {
+        outerHTML += el.outerHTML;
+      });
+
+      this.appendInnerHTML(outerHTML);
+    } else if (content instanceof HTMLElement) {
+      this.appendInnerHTML(content.outerHTML);
+    } else {
+      this.appendInnerHTML(content);
+    }
   }
 }
 

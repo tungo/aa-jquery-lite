bfba094d265c8153e23069037f7106adcf72e706
Tu Ngo <tungo8810@gmail.com>
2017-05-04 16:48:21 -0700

    Implement document ready callback queue

--- jQueryLite/lib/jquery_lite.js
+++ jQueryLite/lib/jquery_lite.js
@@ -75,11 +75,23 @@ const DOMNodeCollection = __webpack_require__(1);
 window.$l = function (selector) {
   if (selector instanceof HTMLElement) {
     return new DOMNodeCollection([selector]);
+  } else if (selector instanceof Function) {
+    if (document.readyState === "complete") {
+      return selector();
+    } else {
+      this.callbackQueue.push(selector);
+      return;
+    }
   }
 
   const elems = Array.from(document.querySelectorAll(selector));
   return new DOMNodeCollection(elems);
 };
+
+window.callbackQueue = [];
+document.addEventListener("DOMContentLoaded", () => {
+  window.callbackQueue.forEach((el) => el());
+});
 
 
 /***/ }),
--- jQueryLite/lib/main.js
+++ jQueryLite/lib/main.js
@@ -3,8 +3,20 @@ const DOMNodeCollection = require("./dom_node_collection.js");
 window.$l = function (selector) {
   if (selector instanceof HTMLElement) {
     return new DOMNodeCollection([selector]);
+  } else if (selector instanceof Function) {
+    if (document.readyState === "complete") {
+      return selector();
+    } else {
+      this.callbackQueue.push(selector);
+      return;
+    }
   }
 
   const elems = Array.from(document.querySelectorAll(selector));
   return new DOMNodeCollection(elems);
 };
+
+window.callbackQueue = [];
+document.addEventListener("DOMContentLoaded", () => {
+  window.callbackQueue.forEach((el) => el());
+});
